cb025b2f957e3965bf05bda6f972f59ef1a2d699
jaisonrobson <xd_jrg@hotmail.com>
2022-02-14 20:52:24 -0300

    feat: feito testes iniciais na parte principal do aplicativo

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { VictoryChart, VictoryLine, VictoryTheme, VictoryScatter, VictoryLabel, VictoryTooltip, VictoryVoronoiContainer } from 'victory-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <VictoryChart
+        containerComponent={<VictoryVoronoiContainer />}
+      >
+        <VictoryLine
+          style={{
+            data: { stroke: "#c43a31" },
+            parent: { border: "1px solid #ccc" }
+          }}
+          data={[
+            { x: 1, y: 2 },
+            { x: 2, y: 3 },
+            { x: 3, y: 5 },
+            { x: 4, y: 4 },
+            { x: 5, y: 7 }
+          ]}
+        />
+        <VictoryScatter
+          style={{ data: { fill: "#c43a31" }, labels: { fill: "black", fontSize: 18 } }}
+          labels={({ datum }) => `x: ${datum.x}, y: ${datum.y}`}
+          labelComponent={<VictoryTooltip />}
+          size={7}
+          data={[
+            { x: 1, y: 2 },
+            { x: 2, y: 3 },
+            { x: 3, y: 5 },
+            { x: 4, y: 4 },
+            { x: 5, y: 7 }
+          ]}
+        />
+      </VictoryChart>
+
+
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +50,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
