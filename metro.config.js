e6fbf6d4c0d34467f3fad6b8477fa9d40c421eb1
jaisonrobson <xd_jrg@hotmail.com>
2022-02-14 20:48:57 -0300

    build: permitido arquivos .jsx na build do projeto

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,10 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+// module.exports = getDefaultConfig(__dirname);
+
+module.exports = (async () => {
+    const { resolver: { sourceExts } } = await getDefaultConfig(__dirname);
+
+    return { resolver: { sourceExts: [...sourceExts, 'jsx'] } };
+})();
