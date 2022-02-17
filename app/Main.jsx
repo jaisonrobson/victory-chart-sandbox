43bba7e8aba0acc7579a62bb9de58bf7e3d12731
jaisonrobson <xd_jrg@hotmail.com>
2022-02-16 23:18:45 -0300

    feat: incluido projecao de objetivo no ScatterLineChart & preparado componente para receber comparativo de conjuntos de dados extensos

--- app/Main.jsx
+++ app/Main.jsx
@@ -10,11 +10,18 @@ const Main = () => (
             zoomDimension="x"
             zoomBrushPercentualSize={50}
             zoomBrushAxisValuesExtractor={(item) => item.x}
-            data={[
-                { x: 1, y: 1 },
-                { x: 2, y: 2 },
-                { x: 3, y: 2 },
-                { x: 4, y: 1 }
+            dataSets={[
+                [
+                    { x: 1, y: 1 },
+                    { x: 2, y: 2 },
+                    { x: 3, y: 2 },
+                    { x: 4, y: 1 }
+                ]
+            ]}
+            projectionData={[
+                { x: 0, y: 3 },
+                { x: 2.5, y: 3 },
+                { x: 5, y: 3 },
             ]}
         />
     </View>
--- app/components/ScatterLineChart.jsx
+++ app/components/ScatterLineChart.jsx
@@ -10,6 +10,7 @@ import {
     VictoryTooltip,
     VictoryBrushContainer,
     VictoryVoronoiContainer,
+    VictoryArea,
     createContainer,
 } from 'victory-native'
 
@@ -26,18 +27,26 @@ const ScatterLineChart = ({
     zoomBrushPercentualSize = 30,
     zoomBrushAxisValuesExtractor = (value) => value,
     zoomBrushAxisFormatter = (value) => value,
+    xDataItemValueExtractor = (item) => item.x,
+    yDataItemValueExtractor = (item) => item.y,
     scale = undefined,
-    data: dataProp = [],
+    dataSets = [[]],
+    projectionData = [],
     style = {},
     ...props
 }) => {
-    const [data, setData] = useState(dataProp)
     const [zoomDomain, setZoomDomain] = useState({})
     const [selectedDomain, setSelectedDomain] = useState({})
 
     const getDomain = () => ({
-        x: [_.minBy(data, value => value.x).x - 2, _.maxBy(data, value => value.x).x + 2],
-        y: [_.minBy(data, value => value.y).y - 2, _.maxBy(data, value => value.y).y + 2],
+        x: [
+            xDataItemValueExtractor(_.minBy(dataSets, (dataSet) => xDataItemValueExtractor(_.minBy(dataSet, (item) => xDataItemValueExtractor(item))))) - 2,
+            xDataItemValueExtractor(_.maxBy(dataSets, (dataSet) => xDataItemValueExtractor(_.maxBy(dataSet, (item) => xDataItemValueExtractor(item))))) + 2
+        ],
+        y: [
+            yDataItemValueExtractor(_.minBy(dataSets, (dataSet) => yDataItemValueExtractor(_.minBy(dataSet, (item) => yDataItemValueExtractor(item))))) - 2,
+            yDataItemValueExtractor(_.maxBy(dataSets, (dataSet) => yDataItemValueExtractor(_.maxBy(dataSet, (item) => yDataItemValueExtractor(item))))) + 2
+        ],
     })
 
     const onChangeZoomDomain = (domain) =>
@@ -49,23 +58,32 @@ const ScatterLineChart = ({
     const isDimensionalZoomEnabled = () => _.toLower(zoomDimension) === 'x' || _.toLower(zoomDimension) === 'y'
 
     const calculateBrushMaximumPercentualSize = () => (
-        Math.abs(zoomBrushAxisValuesExtractor(_.minBy(data, zoomBrushAxisValuesExtractor)))
-        + Math.abs(zoomBrushAxisValuesExtractor(_.maxBy(data, zoomBrushAxisValuesExtractor)))
+        Math.abs(zoomBrushAxisValuesExtractor(_.minBy(dataSets, (dataSet) => zoomBrushAxisValuesExtractor(_.minBy(dataSet, (item) => zoomBrushAxisValuesExtractor(item))))))
+        + Math.abs(zoomBrushAxisValuesExtractor(_.maxBy(dataSets, (dataSet) => zoomBrushAxisValuesExtractor(_.maxBy(dataSet, (item) => zoomBrushAxisValuesExtractor(item))))))
         * (zoomBrushPercentualSize / 100)
+    )
+
+    const calculateBrushMinimumPercentualSize = () => (
+        zoomBrushAxisValuesExtractor(
+            _.minBy(
+                dataSets,
+                (dataSet) => zoomBrushAxisValuesExtractor(_.minBy(dataSet, (item) => zoomBrushAxisValuesExtractor(item)))
+            )
+        )
     )
 
     const getBrushDomain = () =>
         _.toLower(zoomDimension) === 'x'
             ? ({
                 x: [
-                    zoomBrushAxisValuesExtractor(_.minBy(data, zoomBrushAxisValuesExtractor)),
+                    calculateBrushMinimumPercentualSize(),
                     calculateBrushMaximumPercentualSize()
                 ]
             })
             :
             ({
                 y: [
-                    zoomBrushAxisValuesExtractor(_.minBy(data, zoomBrushAxisValuesExtractor)),
+                    calculateBrushMinimumPercentualSize(),
                     calculateBrushMaximumPercentualSize()
                 ]
             })
@@ -89,30 +107,55 @@ const ScatterLineChart = ({
                 scale={scale}
                 {...props}
             >
-                <VictoryLine
+                <VictoryArea
                     style={{
-                        data: { stroke: "#c43a31" },
-                        parent: { border: "1px solid #ccc" },
-                        ...lineStyle
+                        data: {
+                            fill: '#c43a31',
+                            fillOpacity: 0.2,
+                            stroke: '#c43a31',
+                            strokeWidth: 3,
+                            strokeDasharray: 5,
+                        }
                     }}
-                    data={data}
-                    domain={getDomain()}
-                    {...lineProps}
+                    data={projectionData}
+                    labels={() => 'Projection'}
                 />
 
-                <VictoryScatter
-                    style={{
-                        data: { fill: "#c43a31" },
-                        labels: { fill: "black", fontSize: 18 },
-                        ...scatterStyle
-                    }}
-                    labels={({ datum }) => `x: ${datum.x}, y: ${datum.y}`}
-                    labelComponent={<VictoryTooltip />}
-                    size={7}
-                    data={data}
-                    domain={getDomain()}
-                    {...scatterProps}
-                />
+                {
+                    _.map(
+                        dataSets,
+                        (dataSet) => {
+                            return (
+                                <>
+                                    <VictoryLine
+                                        style={{
+                                            data: { stroke: "#c43a31" },
+                                            parent: { border: "1px solid #ccc" },
+                                            ...lineStyle
+                                        }}
+                                        data={dataSet}
+                                        domain={getDomain()}
+                                        {...lineProps}
+                                    />
+
+                                    <VictoryScatter
+                                        style={{
+                                            data: { fill: "#c43a31" },
+                                            labels: { fill: "black", fontSize: 18 },
+                                            ...scatterStyle
+                                        }}
+                                        labels={({ datum }) => `x: ${datum.x}, y: ${datum.y}`}
+                                        labelComponent={<VictoryTooltip />}
+                                        size={7}
+                                        data={dataSet}
+                                        domain={getDomain()}
+                                        {...scatterProps}
+                                    />
+                                </>
+                            )
+                        }
+                    )
+                }
             </VictoryChart>
 
             {
@@ -132,6 +175,12 @@ const ScatterLineChart = ({
                                 />
                             }
                         >
+                            {/* PARA SABER A MAIOR RETA DE EIXO ENTRE OS DATASETS,
+                                FAZER UM SOMATORIO DOS VALORES ABSOLUTOS EM CADA DATASET E
+                                POR FIM, COMPARAR QUAL A MAIOR RETA, COM ISSO A POSICAO DE VETOR,
+                                CUJO ESTE POSSUIR A MAIOR RETA SERA O VETOR VENCEDOR PARA EXIBICAO NO
+                                EIXO DE VALORES DO BRUSH DO ZOOM
+                            */}
                             <VictoryAxis
                                 tickValues={
                                     _.isEmpty(zoomBrushAxisValues)
@@ -141,6 +190,9 @@ const ScatterLineChart = ({
                                 tickFormat={zoomBrushAxisFormatter}
                             />
 
+                            {/*
+                                EXIBIR AQUI APENAS OS LINE CHARTS DE CADA DATASET
+                            */}
                             <VictoryLine
                                 style={{
                                     data: { stroke: "#c43a31" },
